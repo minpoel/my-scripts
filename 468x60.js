a5da06d591c921ea5dc39b9b661546a30b70d677
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-19 10:51:11 +0700

    Update 468x60.js

--- 468x60.js
+++ 468x60.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : '85b78cb696716221641cdf84c08b97c8',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/85b78cb696716221641cdf84c08b97c8/invoke.js"></scr' + 'ipt>');
+
