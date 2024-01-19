2d36c25da630ad7bf510d12eca9d90977cdce1e0
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-19 10:41:00 +0700

    Update 300x2501.js

--- 300x2501.js
+++ 300x2501.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : '865e6e088e9845f5c01d6dffe48743c0',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://goodnesstamper.com/865e6e088e9845f5c01d6dffe48743c0/invoke.js"></scr' + 'ipt>');
+
