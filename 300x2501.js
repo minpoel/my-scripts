a8f2f4af05927b0686063bf07af4a32f1ea09758
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-19 10:48:12 +0700

    Update 300x2501.js

--- 300x2501.js
+++ 300x2501.js
@@ -1,1 +1,8 @@
-
+	atOptions = {
+		'key' : 'd358c597622e21b4a384b929e1954c41',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/d358c597622e21b4a384b929e1954c41/invoke.js"></scr' + 'ipt>');
