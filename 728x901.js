60653c85719e0646145c609e42e9fde080b3a8ee
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-19 10:42:40 +0700

    Update 728x901.js

--- 728x901.js
+++ 728x901.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : '5d3d6d7ae1891cd51a6287e96b5c6a59',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://goodnesstamper.com/5d3d6d7ae1891cd51a6287e96b5c6a59/invoke.js"></scr' + 'ipt>');
+
