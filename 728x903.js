fcf0ddc32d49a899f5e346595df8830d266f93b9
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-19 10:51:41 +0700

    Update 728x903.js

--- 728x903.js
+++ 728x903.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : 'f318b822b118bc96bb23720507ee6f76',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplayformat.com/f318b822b118bc96bb23720507ee6f76/invoke.js"></scr' + 'ipt>');
+
