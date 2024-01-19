b5a98e531a04cf3904b29792582df3a2ac07bd94
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-19 10:51:20 +0700

    Update 728x90.js

--- 728x90.js
+++ 728x90.js
@@ -1,8 +1,1 @@
-	atOptions = {
-		'key' : 'd7b7f7e4f7bf0efe612e38a1418abdc5',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/d7b7f7e4f7bf0efe612e38a1418abdc5/invoke.js"></scr' + 'ipt>');
+
