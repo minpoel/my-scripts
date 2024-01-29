9163911c491f3d33fb0a94b4edbfe83568434ed4
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-29 12:02:57 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -5,4 +5,4 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/f21e123b284186af9fc9cf34430bda7d/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="//deadmentionsunday.com/f21e123b284186af9fc9cf34430bda7d/invoke.js"></scr' + 'ipt>');
