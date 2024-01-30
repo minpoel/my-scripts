7e6dcb5b4564033f5c997728bf2fa751e13b8ea1
minpoel <150368721+minpoel@users.noreply.github.com>
2024-01-30 10:53:00 +0700

    Update 300x2501.js

--- 300x2501.js
+++ 300x2501.js
@@ -1,8 +1,1 @@
-atOptions = {
-		'key' : 'd358c597622e21b4a384b929e1954c41',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/d358c597622e21b4a384b929e1954c41/invoke.js"></scr' + 'ipt>');
+
