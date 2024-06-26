e023d39db2fa3a37d6f1b0cf8d5c6e7645376271
minpoel <150368721+minpoel@users.noreply.github.com>
2024-06-27 01:17:05 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -9,17 +9,10 @@ var atOptions = {
 (function() {
 
     var center = document.createElement('center');
-    
-
     var adContainer = document.createElement('div');
     adContainer.id = 'ad-container';
-    
-
     center.appendChild(adContainer);
-
     document.body.appendChild(center);
-
-
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = '//www.topcreativeformat.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js';
