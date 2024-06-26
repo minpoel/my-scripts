4482e1879c961d3de40ec617552f09468448e5b0
minpoel <150368721+minpoel@users.noreply.github.com>
2024-06-26 09:57:26 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -5,4 +5,5 @@
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js"></scr' + 'ipt>');
+</script>
+<script type="text/javascript" src="//www.topcreativeformat.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js">
