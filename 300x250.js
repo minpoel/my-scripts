37d71336e6d33025a865e00eddc11b843ed293d9
minpoel <150368721+minpoel@users.noreply.github.com>
2024-06-26 10:04:51 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -1,8 +1,14 @@
-	atOptions = {
-		'key' : '2c5ba87416a72de7cfaf34b8cbb03b7a',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-<script type="text/javascript" src="//www.topcreativeformat.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js">
+atOptions = {
+    'key' : '2c5ba87416a72de7cfaf34b8cbb03b7a',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+};
+
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//www.topcreativeformat.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js';
+    document.body.appendChild(script);
+})();
