20dd4b980530d9491e8e6036ac78878ac5834af0
minpoel <150368721+minpoel@users.noreply.github.com>
2024-06-26 10:20:05 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -7,8 +7,20 @@ var atOptions = {
 };
 
 (function() {
+
+    var adContainer = document.createElement('div');
+    adContainer.style.position = 'fixed';
+    adContainer.style.top = '50%';
+    adContainer.style.left = '50%';
+    adContainer.style.transform = 'translate(-50%, -50%)';
+    adContainer.style.width = atOptions.width + 'px';
+    adContainer.style.height = atOptions.height + 'px';
+    adContainer.style.zIndex = '9999'; // Pastikan iklan muncul di atas konten lain
+
+    document.body.appendChild(adContainer);
+
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = '//www.topcreativeformat.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js';
-    document.body.appendChild(script);
+    adContainer.appendChild(script);
 })();
