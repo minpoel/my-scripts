e26e00e75fb9af97e6578b5015b92a8c38354725
minpoel <150368721+minpoel@users.noreply.github.com>
2024-07-15 10:28:57 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -1,20 +1,8 @@
-var atOptions = {
-    'key': '2c5ba87416a72de7cfaf34b8cbb03b7a',
-    'format': 'iframe',
-    'height': 250,
-    'width': 300,
-    'params': {}
-};
-
-(function() {
-
-    var center = document.createElement('center');
-    var adContainer = document.createElement('div');
-    adContainer.id = 'ad-container';
-    center.appendChild(adContainer);
-    document.body.appendChild(center);
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = '//www.topcreativeformat.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js';
-    adContainer.appendChild(script);
-})();
+atOptions = {
+		'key' : '2c5ba87416a72de7cfaf34b8cbb03b7a',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://nauseousrocketjosephine.com/2c5ba87416a72de7cfaf34b8cbb03b7a/invoke.js"></scr' + 'ipt>');
