783c91855b10cb3f19a108f52838723179656772
minpoel <150368721+minpoel@users.noreply.github.com>
2024-07-30 14:07:34 +0700

    Update float.js

--- float.js
+++ float.js
@@ -2,17 +2,3 @@ function closeBanner() {
   const banner = document.getElementById("floating-banner");
   banner.style.display = "none"; 
 }
-
-// Fungsi untuk mengatur ukuran banner
-function resizeBanner() {
-  const banner = document.getElementById("floating-banner");
-  const adElement = document.querySelector(".adsbygoogle");
-
-  if (adElement) {
-    banner.style.width = adElement.offsetWidth + "px";
-    banner.style.height = adElement.offsetHeight + "px";
-  }
-}
-
-// Panggil resizeBanner setelah iklan dimuat
-window.addEventListener('load', resizeBanner); 
