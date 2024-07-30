6cca65df67e089acfc9cae357d097641550a8449
minpoel <150368721+minpoel@users.noreply.github.com>
2024-07-30 14:48:50 +0700

    Update float.js

--- float.js
+++ float.js
@@ -1,7 +1,6 @@
-// Script untuk mengontrol tampilan banner
-    const bannerContainer = document.querySelector('.banner-container');
-    const closeBtn = document.querySelector('.close-btn');
+const bannerContainer = document.querySelector('.banner-container');
+const closeBtn = document.querySelector('.close-btn');
 
-    closeBtn.addEventListener('click', () => {
-      bannerContainer.style.bottom = '-100%'; // Menghilangkan banner
-    });
+closeBtn.addEventListener('click', () => {
+  bannerContainer.style.bottom = '-100%'; 
+});
