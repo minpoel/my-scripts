8cbc8a380c8118c26ef73c32bc63c9014e45981d
minpoel <150368721+minpoel@users.noreply.github.com>
2024-07-30 11:24:40 +0700

    Update float.js

--- float.js
+++ float.js
@@ -1,21 +1,3 @@
-// Script banner responsif
-function adjustBannerSize() {
-  var banner = document.getElementById('floating-banner');
-  var maxWidth = window.innerWidth * 0.8; // 80% dari lebar layar
-  var maxHeight = window.innerHeight * 0.8; // 80% dari tinggi layar
-
-  // Set
-  banner.style.maxWidth = Math.min(maxWidth, maxHeight) + 'px';
+function closeBanner() {
+  document.getElementById("floating-banner").style.display = "none";
 }
-
-// call
-adjustBannerSize();
-
-// call
-window.addEventListener('resize', adjustBannerSize);
-
-// Event listener close
-document.getElementById('close-btn').addEventListener('click', function() {
-  var banner = document.getElementById('floating-banner');
-  banner.style.display = 'none'; // banner close click
-});
