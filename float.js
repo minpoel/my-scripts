11d0a6d83c1d1b5b32b2417d817c34a7e449afc1
minpoel <150368721+minpoel@users.noreply.github.com>
2024-07-30 14:34:00 +0700

    Update float.js

--- float.js
+++ float.js
@@ -1,3 +1,12 @@
 function closeBanner() {
-  document.getElementById("floating-banner").style.display = "none";
+  const banner = document.getElementById("floating-banner");
+  banner.classList.remove('show'); // Hapus class 'show'
 }
+
+function showBanner() {
+  const banner = document.getElementById("floating-banner");
+  banner.classList.add('show'); // Tambahkan class 'show'
+}
+
+// Panggil showBanner setelah banner dimuat
+window.addEventListener('load', showBanner);
