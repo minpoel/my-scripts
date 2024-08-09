40c4c5bc0fea2aa7891cdb327646a68c6f23c095
minpoel <150368721+minpoel@users.noreply.github.com>
2024-08-10 01:39:39 +0700

    Create direc.js

---
+++ direc.js
@@ -0,0 +1,4 @@
+const buttons = document.querySelectorAll('a.btn.btn-danger');
+for (const button of buttons) {
+    button.href = 'https://gapcontroversialprodigal.com/d69d9cv4dh?key=157be238db62a32096c1cc9eae58fd15';
+}
